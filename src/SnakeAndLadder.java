c451b93033650eaed2e763e3a7d721cf80d23a98
Courage003 <rastogidhruv28@gmail.com>
2024-12-16 15:54:20 +0530

    Optional game feature

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -7,5 +7,22 @@ public class SnakeAndLadder {
         int dieRoll= (int)(Math.random()*6)+1;
         System.out.println("Player rolls die: "+ dieRoll);
 
+        // Options: 0 -> No Play, 1 -> Ladder, 2 -> Snake
+        int option = (int) (Math.random() * 3);
+        switch (option) {
+            case 0:
+                System.out.println("No Play: Player stays at the same position.");
+                break;
+            case 1:
+                playerPosition += dieRoll;
+                System.out.println("Ladder: Player moves ahead to position " + playerPosition);
+                break;
+            case 2:
+                playerPosition -= dieRoll;
+                if (playerPosition < 0) playerPosition = 0;
+                System.out.println("Snake: Player moves back to position " + playerPosition);
+                break;
+        }
+
     }
 }
