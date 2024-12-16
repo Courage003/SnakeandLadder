523c4121d2934a993df150108ca78fe0d056de61
Courage003 <rastogidhruv28@gmail.com>
2024-12-16 15:49:43 +0530

    Player poition update

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadder {
+    public static void main(String[] args){
+        System.out.println("Welcome to Snake and Ladder Simulator");
+
+        int playerPosition =0;
+        System.out.println("Player starts at position: "+ playerPosition);
+
+    }
+}
